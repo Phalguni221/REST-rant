d4722dac7acf281d80c2ec6cb9a0d01bd5a5ff79
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2021-12-21 19:24:58 -0500

    Adding default view for layout

---
+++ Views/default.jsx
@@ -0,0 +1,16 @@
+function Def (html) {
+    return (
+        <html>
+           <head>
+  <title>Hello, world!</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width,initial-scale=1" />
+  <meta name="description" content="" />
+  <link rel="stylesheet" href="style.css"/>
+</head>
+            <body>
+                {html.children}
+            </body>
+        </html>
+    )
+  }
