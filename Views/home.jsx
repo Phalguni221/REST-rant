262f40bfa13a6792542a7419d1f250afc7cac70c
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2022-01-14 20:07:12 -0500

    REST-RANT

--- Views/home.jsx
+++ Views/home.jsx
@@ -12,7 +12,8 @@ const Def = require('./default')
     <img src="https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9vZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" 
     alt="Walnut Salad"/>
                     </div>
-        Photo by <a href="https://unsplash.com/@shootdelicious">Eiliv-Sonas Aceron</a> on <a href="https://unsplash.com/s/photos/free">Unsplash</a>
+       Photo by <a href="https://unsplash.com/@shootdelicious">Eiliv-Sonas Aceron</a> on <a href="https://unsplash.com/s/photos/free">Unsplash</a>
+            <br></br>
             <br></br>
             <div class = "row">
 
@@ -40,10 +41,11 @@ const Def = require('./default')
                     <img src="./Images/Kawaii Kitty Cafe.png" alt="Kawaii Kitty Cafe"/>
                 </div> 
                 <br></br>
+            </div>
+                <h4>Learn more about each place!!</h4>
               <a href="/places">
   <button className="btn-primary">Places Page</button>
               </a>
-        </div>
               </main>
         </Def>
         )
