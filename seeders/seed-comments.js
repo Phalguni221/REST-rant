fe3d7a18e3eab7897f68b7db14fbe2d681bc219f
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2022-01-14 22:27:53 -0500

    REST-RANT

--- seeders/seed-comments.js
+++ seeders/seed-comments.js
@@ -15,6 +15,9 @@ async function seed() {
 
     // Add that comment to the place's comment array.
     place.comments.push(comment.id)
+
+    //Add .save()
+    await place.save()
     
     // Exit the program
     process.exit()
