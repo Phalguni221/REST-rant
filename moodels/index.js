b0954ebd27f6690a3ae5a54cfd3e1b9501d3cabb
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2022-01-23 18:24:59 -0500

    renaming part 2

--- moodels/index.js
+++ moodels/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require('dotenv').config()
 const mongoose = require('mongoose')
 
@@ -9,16 +8,4 @@ mongoose.connect(process.env.MONGO_URI, {
 
 module.exports.Place = require('./places')
 
-=======
-require('dotenv').config()
-const mongoose = require('mongoose')
-
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true, 
-  useUnifiedTopology: true
-})
-
-module.exports.Place = require('./places')
-
->>>>>>> f662c9b18a56b0637cdefb8f21cb57891c0d4d3d
 module.exports.Comment = require('./comment')
