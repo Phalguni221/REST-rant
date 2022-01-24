9b40c92fe12988e329fe317fae43f63c1486be58
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2022-01-23 22:30:45 -0500

    rest-rant-1

--- views/home.jsx
+++ views/home.jsx
@@ -4,11 +4,11 @@ const Def = require('./default')
     function home () {
         return (
           <Def>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"/>
+            <link rel="stylesheet" href="/css/style.css"/>
               <main>
                   <head>
                        <h1>REST-RANT</h1>
-                       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"/>
-            <link rel="stylesheet" href="/css/style.css"/>
                   </head>
                   <div className="col-sm-6">
     <img src="https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zm9vZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" 
