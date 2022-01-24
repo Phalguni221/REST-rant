911ff01d8e09938911d3c337fc8f6d09028e416c
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2022-01-23 20:28:13 -0500

    final renaming(hopefully)

--- models/comment.js
+++ models/comment.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require('mongoose')
 
 let commentSchema = new mongoose.Schema({
@@ -10,17 +9,4 @@ let commentSchema = new mongoose.Schema({
 
 commentSchema.methods.showEstablished = function() {}
   
-=======
-const mongoose = require('mongoose')
-
-let commentSchema = new mongoose.Schema({
-    author: { type: String, default: 'Anonymous' },
-    rant: { type: Boolean, default: false },
-    stars: { type: Number, required: true },
-    content: { type: String, default: '' }
-})
-
-commentSchema.methods.showEstablished = function() {}
-  
->>>>>>> f662c9b18a56b0637cdefb8f21cb57891c0d4d3d
 module.exports = mongoose.model('Comment', commentSchema)
